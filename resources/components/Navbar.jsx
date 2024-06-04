2bed002376b4622091d4654d05be36b3df087df1
Annyotamo Barman <abprogresss@gmail.com>
2024-06-04 12:41:32 +0530

    added hover effects

--- resources/components/Navbar.jsx
+++ resources/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import styles from "../css/Navbar.module.css"
 import logoImage from "../assets/Logo.png";
 import Image from "next/image";
-import Link from "next/link";
 export default function Navbar() {
     return (
         <div className={styles["navbar-container"]}>
@@ -9,7 +8,7 @@ export default function Navbar() {
                 <Image src={logoImage} />
                 <ul>
                     <li>About Us</li>
-                    <Link href="/pricing"><li>Pricing</li></Link>
+                    <li>Pricing</li>
                     <li>Features</li>
                     <li>Home</li>
                 </ul>
