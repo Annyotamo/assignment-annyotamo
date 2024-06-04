93a655980c7ce81c0aaa71a937e14b5ed418e5cf
Annyotamo Barman <abprogresss@gmail.com>
2024-06-04 12:47:09 +0530

    added favicon

--- app/pricing/layout.js
+++
@@ -1,3 +0,0 @@
-export default function PricingLayout({ children }) {
-    return <main>{children}</main>;
-}
--- app/pricing/page.js
+++
@@ -1,9 +0,0 @@
-import Section1 from "@/resources/components/Section 1/Section1";
-
-export default function PricingPage() {
-    return (
-        <div>
-            <Section1 />
-        </div>
-    );
-}
